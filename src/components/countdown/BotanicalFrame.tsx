8f8ce33885375ceff51deede9a49b253738f11ad
henryrnovoa01 <henry.r.novoa@gmail.com>
2025-06-29 20:18:57 +0200

    put logo in the middle

--- src/components/countdown/BotanicalFrame.tsx
+++ src/components/countdown/BotanicalFrame.tsx
@@ -1,12 +1,14 @@
 export function BotanicalFrame() {
   return (
     <div className="absolute inset-0 pointer-events-none">
-      {/* Top left foliage */}
-      <img
-        src="https://cdn.jsdelivr.net/gh/HenryNovoa/rsvp-images@main/logo.svg"
-        alt=""
-        className="absolute -top-28 right-32 w-32 h-32 object-cover transform"
-      />
+      {/* Top center foliage */}
+      <div className="flex justify-center absolute -top-24 left-0 w-full">
+        <img
+          src="https://cdn.jsdelivr.net/gh/HenryNovoa/rsvp-images@main/logo.svg"
+          alt=""
+          className="w-32 h-32 object-cover"
+        />
+      </div>
       {/* Decorative frame */}
       <div className="absolute inset-8 border-2 border-[#8FA98F]/20 rounded-lg transform rotate-3" />
       <div className="absolute inset-8 border-2 border-[#8FA98F]/20 rounded-lg transform -rotate-3" />
