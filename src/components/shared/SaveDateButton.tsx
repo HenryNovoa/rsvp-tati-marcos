53258f12642dd113b21da258965caf25b252d283
henryrnovoa01 <henry.r.novoa@gmail.com>
2025-07-17 20:39:37 +0200

    change on save time

--- src/components/shared/SaveDateButton.tsx
+++ src/components/shared/SaveDateButton.tsx
@@ -6,7 +6,7 @@ export function SaveDateButton() {
   const handleSaveDate = () => {
     const eventDetails = {
       title: "Boda de Tati & Marcos",
-      startDate: new Date("2025-10-19T14:30:00"),
+      startDate: new Date("2025-10-19T13:30:00"),
       endDate: new Date("2025-10-19T23:59:00"),
       description: "Acompáñanos en nuestra boda",
       location: "Pl. de les Glòries Catalanes, 3, Sant Fost de Campestelles",
